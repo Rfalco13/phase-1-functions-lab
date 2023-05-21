6c6820d52bf43637cea52516916bfe395f36e323
ryanfalco13@gmail.com <ryanfalco13@gmail.com>
2023-05-21 17:08:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42;
+    const distanceInBlocks = Math.abs(pickupLocation - hqLocation);
+    return distanceInBlocks;
+  }
+  
+  function distanceFromHqInFeet(pickupLocation) {
+    const feetPerBlock = 264;
+    const distanceInBlocks = distanceFromHqInBlocks(pickupLocation);
+    const distanceInFeet = distanceInBlocks * feetPerBlock;
+    return distanceInFeet;
+  }
+  
+  function distanceTravelledInFeet(startLocation, destinationLocation) {
+    const feetPerBlock = 264;
+    const distanceInBlocks = Math.abs(startLocation - destinationLocation);
+    const distanceInFeet = distanceInBlocks * feetPerBlock;
+    return distanceInFeet;
+  }
+  
+  function calculatesFarePrice(startLocation, destinationLocation) {
+    const distanceInFeet = distanceTravelledInFeet(startLocation, destinationLocation);
+    
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      const farePrice = (distanceInFeet - 400) * 0.02;
+      return farePrice;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
